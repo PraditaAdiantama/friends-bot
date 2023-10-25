bb98e97afbb96906e7dc6c3fbad04e5be1ab2bd8
unknown <aditadiantama@gmail.com>
2023-10-25 10:54:28 +0700

    feat: add new dev

--- src/validations/dev-only.js
+++ src/validations/dev-only.js
@@ -1,8 +1,11 @@
-module.exports = (interaction,commandObj) => {
-    if(commandObj.devOnly){
-        if(interaction.member.id !== '793991682310799360'){
-            interaction.reply('Command ini hanya bisa di gunakan developer')
-            return true;
-        }
+module.exports = (interaction, commandObj) => {
+  if (commandObj.devOnly) {
+    if (interaction.member.id !== "793991682310799360") {
+      interaction.reply("Command ini hanya bisa di gunakan developer");
+      return true;
+    } else if (interaction.member.id !== "1166584479897354251") {
+      interaction.reply("Command ini hanya bisa di gunakan developer");
+      return true;
     }
-}
+  }
+};
